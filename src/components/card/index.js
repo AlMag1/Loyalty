68fe0fce60163ff5377eec3ec117413a1ae456cf
Aris Alexopoulos <alex.aris90@gmail.com>
2021-11-04 15:55:52 +0200

    Create card component

---
+++ src/components/card/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { colors } from '../../variables';
+
+const Benefits = ({ image }) => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <View style={[styles.card, { width: width - 64 }]}>
+      <Image source={image} style={styles.image} />
+      <View style={styles.relativeContainer}>
+        <View style={styles.absoluteContainer}>
+          <View>
+            <Text style={styles.primaryText}>10 Πόντοι για κάθε 1Ε αγορών</Text>
+            <Text style={styles.secondaryText}>
+              Κέρδισε πόντους και εξαργύρωσε τους!
+            </Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <Ionicons
+              name="chevron-forward-outline"
+              size={15}
+              color={colors.white}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Benefits;
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 24,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginHorizontal: 16,
+    height: 190,
+  },
+  image: { width: '100%', height: '70%' },
+  relativeContainer: {
+    backgroundColor: colors.white,
+    height: '30%',
+    position: 'relative',
+    width: '100%',
+  },
+  absoluteContainer: {
+    flexDirection: 'row',
+    backgroundColor: colors.white,
+    position: 'absolute',
+    left: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 0,
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+    maxWidth: '90%',
+    marginBottom: 8,
+  },
+  primaryText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: colors.greyMid,
+  },
+  iconContainer: {
+    backgroundColor: colors.blue,
+    borderRadius: 30,
+    width: 20,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './button';
 export { default as Logo } from './logo';
 export { default as FormField } from './formField';
+export { default as Card } from './card';
