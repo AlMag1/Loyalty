e3aa0d59e95ccd26628cc61879986a36f9e78a37
Aris Alexopoulos <alex.aris90@gmail.com>
2021-11-04 16:09:19 +0200

    Create win now component

---
+++ src/views/myCollection/components/winNow/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Card } from '../../../../components';
+import { LivingRoomImage } from '../../../../assets';
+
+const WinNow = () => {
+  return <Card image={LivingRoomImage} />;
+};
+
+export default WinNow;
+
+const styles = StyleSheet.create({});
